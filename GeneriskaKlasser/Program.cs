dc9a4091c3b07041512a973be367756155faf1ce
William-Wolke <60339769+William-Wolke@users.noreply.github.com>
2021-03-11 16:09:46 +0100

    initial commit

---
+++ GeneriskaKlasser/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GeneriskaKlasser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> goods = new Dictionary<string, int>();
+
+            goods.Add("popcorn", 20);
+        }
+    }
+}
