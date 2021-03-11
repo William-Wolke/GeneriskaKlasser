08a8c1454fb3c4b34699d238b2704c01edfd927a
William-Wolke <60339769+William-Wolke@users.noreply.github.com>
2021-03-11 16:28:54 +0100

    inte mycket gjort tyvärr :(

--- GeneriskaKlasser/Program.cs
+++ GeneriskaKlasser/Program.cs
@@ -10,6 +10,19 @@ namespace GeneriskaKlasser
             Dictionary<string, int> goods = new Dictionary<string, int>();
 
             goods.Add("popcorn", 20);
+            goods.Add("banana", 10);
+            goods.Add("keso", 25);
+            goods.Add("vegan bratwurst", 30);
+            goods.Add("taco sauce", 35);
+            goods.Add("frozen shrimp", 50);
+            goods.Add("sambal oelek", 25);
+            goods.Add("billys pizza", 15);
+            goods.Add("gorbys pirog", 15);
+            goods.Add("grill spice", 40);
+            goods.Add("basmati rice", 50);
+            goods.Add("caramel sauce", 15);
+
+            System.Console.WriteLine("popcorn pris: " + goods["popcorn"]);
         }
     }
 }
