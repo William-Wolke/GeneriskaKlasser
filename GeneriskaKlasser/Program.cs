dc82ef207a936095504e6904953332d4ecacb533
William-Wolke <60339769+William-Wolke@users.noreply.github.com>
2021-05-11 13:54:40 +0200

    typ klar med alla generiska klasser

--- GeneriskaKlasser/Program.cs
+++ GeneriskaKlasser/Program.cs
@@ -8,6 +8,8 @@ namespace GeneriskaKlasser
         static void Main(string[] args)
         {
             Dictionary<string, int> goods = new Dictionary<string, int>();
+            Queue<string> lootedProducts = new Queue<string>();
+            List<int> checkoutPrice = new List<int>();
 
             goods.Add("popcorn", 20);
             goods.Add("banana", 10);
@@ -22,7 +24,24 @@ namespace GeneriskaKlasser
             goods.Add("basmati rice", 50);
             goods.Add("caramel sauce", 15);
 
-            System.Console.WriteLine("popcorn pris: " + goods["popcorn"]);
+            goods.Remove("gorbys pirog");
+
+            lootedProducts.Enqueue("popcorn");
+            lootedProducts.Enqueue("vegan bratwurst");
+            lootedProducts.Enqueue("sambal oelek");
+            lootedProducts.Enqueue("caramel sauce");
+
+            System.Console.WriteLine("Number of items in cart " + lootedProducts.Count);
+
+            for (int i = 0; i < 4; i++)
+            {
+                checkoutPrice.Add(goods[lootedProducts.Dequeue()]);
+                System.Console.WriteLine("Item price " + checkoutPrice[i]);
+                System.Console.WriteLine("Item " + (i+1) + " of " + 4); 
+            }
+
+            System.Console.WriteLine("popcorn price: " + goods["popcorn"]);
+            System.Console.ReadLine();
         }
     }
 }
